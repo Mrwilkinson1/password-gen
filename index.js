496b27517355fe7014bd96dcbe97691fbf78e643
Alex Wilkinson <mrwilkinson1@gmail.com>
2022-07-04 18:15:50 -0400

    Add for loop and generate

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ var special = '@#$!%^&*+?';
 
 // Write password to the #password input
 function writePassword() {
-
   var myPw = captialCase;
   var passwordLgth = window.prompt ("what length would you like your password")
   // Length 8 to 128
     if (passwordLgth >= 8 && passwordLgth <= 128) { 
        // var password = generatePassword();
-
+        // ASKING A QUESTION USING CONDITIONAL TO = VALUE
     var askCapitalcase = prompt ('Would you like capital case?')
       if (askCapitalcase === 'yes') {
         myPw = myPw;
@@ -33,14 +32,32 @@ function writePassword() {
             console.log('special characters', myPw)
     } 
     
+  } else {
+
+  window.alert('Invalid. Try again');
+  writePassword();
 
 
+}    
+var passwordoption = myPw.split('');
+// CREATE EMPTY ARRAY FOR RESULTS
+const result = [];
 
-
-var passwordText = document.querySelector("#password");
+for (i=0; i < passwordLgth; i++) {
+   var number = Math.floor(Math.random() * myPw.length);
+   
+   var generatePassword = passwordoption[number] ;
+   console.log('number', number, 'character', generatePassword)
+  //  add elements to array
+   result.push(generatePassword)
+ }
+ const newPassword = result.join('')
+ var passwordText = document.querySelector("#password");
 
   passwordText.value = newPassword;
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword());
+
